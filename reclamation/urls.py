d225e371c3cf2c74cf921e027d9828e0b288bc82
Nizar Maltouf <maltouf.nizar@ine.inpt.ma>
2023-04-20 22:42:26 +0000

    commit vercel 71

--- reclamation/urls.py
+++ reclamation/urls.py
@@ -3,7 +3,7 @@ from django.urls import path,include
 from .views import view_form,view_reclamation ,view_reclamation_suivi,view_form_suivi,view_home
 
 urlpatterns = [
-    path("form",view_form),
+    path("form/",view_form),
     path('data/',view_reclamation),
     path('',view_home)
      
